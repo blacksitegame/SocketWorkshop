bc1f7d53361da40084d446abc9721b5bb2c61c45
Jonas-Petersen <jonasvm6@hotmail.com>
2025-08-26 09:57:03 +0200

    TCPClient Opgave2

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,20 +1,32 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Scanner;
 
 public static void main(String argv[]) throws Exception {
     while (true) {
-        String sentence;
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("10.10.131.132", 6789);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + '\n');
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
+        Socket clientSocket = new Socket("10.10.131.157", 6789);
+
+        receive(clientSocket);
+        send(clientSocket);
         clientSocket.close();
     }
 }
+
+public static String receive(Socket connectionSocket) throws IOException {
+    String clientSentence;
+
+    BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+    clientSentence = inFromClient.readLine();
+    return clientSentence;
+}
+
+public static void send(Socket connectionSocket) throws IOException {
+    DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+    Scanner scanner = new Scanner(System.in);
+
+    outToClient.writeBytes(scanner.next());
+}
+
