ad47f59e497103d65fb9a7038adc206b99903ba8
blacksitegame <victor_rasmussen@live.dk>
2025-08-26 11:25:45 +0200

    hej

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,11 +1,7 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Scanner;
 
-public static void main(String argv[]) throws Exception {
+public class TCPClient {
+    public static void main(String argv[]) throws Exception {
         Socket clientSocket = new Socket("10.10.131.157", 6789);
 
         TCPReceiverThread receiverThread = new TCPReceiverThread(clientSocket);
@@ -13,6 +9,5 @@ public static void main(String argv[]) throws Exception {
         receiverThread.start();
         sendThread.start();
 //        clientSocket.close();
+    }
 }
-
-
