8ad97a720e5bf28d7575a2dd4e71a782e9310163
RasWest <83766648+RasWest@users.noreply.github.com>
2025-08-26 10:33:45 +0200

    added class scopes around main method

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -5,28 +5,31 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
 
-public static void main(String argv[]) throws Exception {
-    while (true) {
-        Socket clientSocket = new Socket("10.10.131.157", 6789);
 
-        System.out.println(receive(clientSocket));
-        send(clientSocket);
-        clientSocket.close();
+public class TCPClient {
+    public static void main(String argv[]) throws Exception {
+        while (true) {
+            Socket clientSocket = new Socket("10.10.131.157", 6789);
+
+            System.out.println(receive(clientSocket));
+            send(clientSocket);
+            clientSocket.close();
+        }
     }
-}
 
-public static String receive(Socket connectionSocket) throws IOException {
-    String clientSentence;
+    public static String receive(Socket connectionSocket) throws IOException {
+        String clientSentence;
 
-    BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-    clientSentence = inFromClient.readLine();
-    return clientSentence;
-}
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        clientSentence = inFromClient.readLine();
+        return clientSentence;
+    }
 
-public static void send(Socket connectionSocket) throws IOException {
-    DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-    Scanner scanner = new Scanner(System.in);
+    public static void send(Socket connectionSocket) throws IOException {
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+        Scanner scanner = new Scanner(System.in);
 
-    outToClient.writeBytes(scanner.next());
+        outToClient.writeBytes(scanner.next());
+    }
 }
 
