8d82474ac6e34d5dbf4d951bc18ea65e49371890
Jonas-Petersen <jonasvm6@hotmail.com>
2025-08-26 11:20:34 +0200

    TCPClient Opgave2 - client

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -6,15 +6,13 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public static void main(String argv[]) throws Exception {
-    while (true) {
-        Socket clientSocket = new Socket("10.10.131.157", 6789);
+    Socket clientSocket = new Socket("10.10.131.157", 6789);
 
-        TCPReceiverThread receiverThread = new TCPReceiverThread(clientSocket);
-        TCPSendThread sendThread = new TCPSendThread(clientSocket);
-        receiverThread.start();
-        sendThread.start();
-//        clientSocket.close();
-    }
+    TCPReceiverThread receiverThread = new TCPReceiverThread(clientSocket);
+    TCPSendThread sendThread = new TCPSendThread(clientSocket);
+    receiverThread.start();
+    sendThread.start();
+    //        clientSocket.close();
 }
 
 
